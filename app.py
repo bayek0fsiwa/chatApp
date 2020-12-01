cc7c5a85e5cea65d8c7c14a8d4bf1a6eea1e076e
bayek0fsiwa <imvikas@tutanota.com>
2020-12-01 22:20:20 +0530

    added tpl

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask
+from flask import app
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello There!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
