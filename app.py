7134b55dcba47c50177182f98b4d0a11209f5f9f
bayek0fsiwa <imvikas@tutanota.com>
2020-12-01 22:48:48 +0530

    flake8

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from flask import app, render_template
-import os
+from flask import Flask, render_template
+# import os
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
