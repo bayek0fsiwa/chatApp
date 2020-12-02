ec0b13d6aa938f5174605cf14b01d0ee52413ef7
bayek0fsiwa <imvikas@tutanota.com>
2020-12-02 12:47:14 +0530

    added chat route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask.helpers import url_for
 # import os
 app = Flask(__name__)
 
@@ -6,6 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.tpl")
+
+
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+    if username and room:
+        return render_template('chat.tpl')
+    else:
+        return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
